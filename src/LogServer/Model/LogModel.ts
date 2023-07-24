bbde4f9c5004a0882ce35e44c2672dc4cb1a8c7d
Admy <dondi1412@gmail.com>
2023-07-25 01:03:35 +0700

    update

--- src/LogServer/Model/LogModel.ts
+++ src/LogServer/Model/LogModel.ts
@@ -1,11 +1,129 @@
 import { LogCode } from "./LogCode";
 
 export enum PlatformCode{
-    Unknow = 0,
-
-    IOS = 1,
-    Android = 2,
-    Window = 3,
+    GameCoreScarlett = -1,
+    //
+    // Summary:
+    //     In the Unity editor on macOS.
+    OSXEditor = 0,
+    //
+    // Summary:
+    //     In the player on macOS.
+    OSXPlayer = 1,
+    //
+    // Summary:
+    //     In the player on Windows.
+    WindowsPlayer = 2,
+    //
+    // Summary:
+    //     In the web player on macOS.
+    OSXWebPlayer = 3,
+    //
+    // Summary:
+    //     In the Dashboard widget on macOS.
+    OSXDashboardPlayer = 4,
+    //
+    // Summary:
+    //     In the web player on Windows.
+    WindowsWebPlayer = 5,
+    //
+    // Summary:
+    //     In the Unity editor on Windows.
+    WindowsEditor = 7,
+    //
+    // Summary:
+    //     In the player on the iPhone.
+    IPhonePlayer = 8,
+    PS3 = 9,
+    XBOX360 = 10,
+    //
+    // Summary:
+    //     In the player on Android devices.
+    Android = 11,
+    NaCl = 12,
+    //
+    // Summary:
+    //     In the player on Linux.
+    LinuxPlayer = 13,
+    FlashPlayer = 15,
+    //
+    // Summary:
+    //     In the Unity editor on Linux.
+    LinuxEditor = 16,
+    //
+    // Summary:
+    //     In the player on WebGL
+    WebGLPlayer = 17,
+    MetroPlayerX86 = 18,
+    //
+    // Summary:
+    //     In the player on Windows Store Apps when CPU architecture is X86.
+    WSAPlayerX86 = 18,
+    MetroPlayerX64 = 19,
+    //
+    // Summary:
+    //     In the player on Windows Store Apps when CPU architecture is X64.
+    WSAPlayerX64 = 19,
+    MetroPlayerARM = 20,
+    //
+    // Summary:
+    //     In the player on Windows Store Apps when CPU architecture is ARM.
+    WSAPlayerARM = 20,
+    WP8Player = 21,
+    BB10Player = 22,
+    BlackBerryPlayer = 22,
+    TizenPlayer = 23,
+    PSP2 = 24,
+    //
+    // Summary:
+    //     In the player on the Playstation 4.
+    PS4 = 25,
+    PSM = 26,
+    //
+    // Summary:
+    //     In the player on Xbox One.
+    XboxOne = 27,
+    SamsungTVPlayer = 28,
+    WiiU = 30,
+    //
+    // Summary:
+    //     In the player on the Apple's tvOS.
+    tvOS = 31,
+    //
+    // Summary:
+    //     In the player on Nintendo Switch.
+    Switch = 32,
+    Lumin = 33,
+    //
+    // Summary:
+    //     In the player on Stadia.
+    Stadia = 34,
+    //
+    // Summary:
+    //     In the player on CloudRendering.
+    CloudRendering = 35,
+    GameCoreXboxSeries = 36,
+    GameCoreXboxOne = 37,
+    //
+    // Summary:
+    //     In the player on the Playstation 5.
+    PS5 = 38,
+    EmbeddedLinuxArm64 = 39,
+    EmbeddedLinuxArm32 = 40,
+    EmbeddedLinuxX64 = 41,
+    EmbeddedLinuxX86 = 42,
+    //
+    // Summary:
+    //     In the server on Linux.
+    LinuxServer = 43,
+    //
+    // Summary:
+    //     In the server on Windows.
+    WindowsServer = 44,
+    //
+    // Summary:
+    //     In the server on macOS.
+    OSXServer = 45
 }
 
 export enum LogType{
