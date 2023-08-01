dddca03daf2fbaad3d23d582334f4e95272d7de1
Admy <dondi1412@gmail.com>
2023-08-01 17:47:40 +0700

    up version

--- src/App.ts
+++ src/App.ts
@@ -20,7 +20,7 @@ import { InitWebServer } from './WebServer';
 
 // Define number of worker processes
 const numCPUs = 1;
-const version = "0.0.11"
+const version = "0.0.12"
 // const numCPUs = require('os').cpus().length;
 // AppTest();
 InitApp();
